fdce9bab4058003a0a64d7a901daf22b9e90c57f
Annet <annet312@gmail.com>
2018-05-29 22:21:45 +0300

    modify DI with Convention over configuration

--- TaskManagerBLL/Infrastructure/ServiceModule.cs
+++ TaskManagerBLL/Infrastructure/ServiceModule.cs
@@ -16,16 +16,12 @@ namespace TaskManagerBLL.Infrastructure
         {
             string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
-
-            //builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces();
-            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().WithParameter("connectionString",connectionString);
-            builder.RegisterType<TaskService>().As<ITaskService>();
-            builder.RegisterType<StatusService>().As<IStatusService>();
-            builder.RegisterType<PersonService>().As<IPersonService>();
-            builder.RegisterType<FilterTaskService>().As<IFilterTaskService>();
-            builder.RegisterType<EmailService>().As<IEmailService>();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().WithParameter("connectionString", connectionString);
+            builder.RegisterAssemblyTypes(typeof(ServiceModule).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                 .AsImplementedInterfaces();
+           
             builder.RegisterAssemblyTypes().AssignableTo(typeof(Profile)).As<Profile>();
-
             builder.Register(c => new MapperConfiguration(cfg =>
             {
                 foreach (var profile in c.Resolve<IEnumerable<Profile>>())
@@ -33,6 +29,6 @@ namespace TaskManagerBLL.Infrastructure
                     cfg.AddProfile(profile);
                 }
             })).AsSelf().SingleInstance();
-            
+        }  
     }
 }
